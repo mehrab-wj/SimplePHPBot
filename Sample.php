d9979d44af4f49827b24e7eb56bcb6576265f01c
mehrab-wj <white_joker20@yahoo.com>
2016-08-01 10:57:07 -0700

    New Update !
    Add 2 Function For Help To You !
    Add Inline Keyboard For training !
    $userText changed to $textmessage !

--- Sample.php
+++ Sample.php
@@ -1,7 +1,12 @@
 <?php
-//Telegram.me/OneProgrammer
-//Telegram.me/SpyGuard
-//----------------------Lotfan Copy Right Ro Rayat Konid <3
+/*
+ Telegram.me/OneProgrammer
+ Telegram.me/SpyGuard
+                   ----[ Lotfan Copy Right Ro Rayat Konid <3 ]----
+############################################################################################
+# if you need Help for develop this source , You Can Send Message To Me With @SpyGuard_BOT #
+############################################################################################
+*/
 define('API_KEY','BOTTOKEN');
 function makereq($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
@@ -16,6 +21,54 @@ function makereq($method,$datas=[]){
         return json_decode($res);
     }
 }
+
+//----######------
+
+function makereq($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+//##############=--API_REQ
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = API_URL.$method.'?'.http_build_query($parameters);
+
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+  return exec_curl_request($handle);
+}
+
+//----######------
 //---------
 $update = json_decode(file_get_contents('php://input'));
 var_dump($update);
@@ -23,10 +76,14 @@ var_dump($update);
 $chat_id = $update->message->chat->id;
 $message_id = $update->message->message_id;
 $from_id = $update->message->from->id;
+$name = $update->message->from->first_name;
 $username = $update->message->from->username;
-$userTEXT = isset($update->message->text)?$update->message->text:'';
+$textmessage = isset($update->message->text)?$update->message->text:'';
+$reply = $update->message->reply_to_message->forward_from->id;
+$stickerid = $update->message->reply_to_message->sticker->file_id;
+
 $admin = 66443035;
-//------
+//-------
 function SendMessage($ChatId, $TextMsg)
 {
  makereq('sendMessage',[
@@ -35,14 +92,53 @@ function SendMessage($ChatId, $TextMsg)
 'parse_mode'=>"MarkDown"
 ]);
 }
-
-if($userTEXT == '/start')
+function SendSticker($ChatId, $sticker_ID)
 {
-SendMessage($chat_id,"Welcome");
+ makereq('sendSticker',[
+'chat_id'=>$ChatId,
+'sticker'=>$sticker_ID
+]);
+}
+function Forward($KojaShe,$AzKoja,$KodomMSG)
+{
+makereq('ForwardMessage',[
+'chat_id'=>$KojaShe,
+'from_chat_id'=>$AzKoja,
+'message_id'=>$KodomMSG
+]);
+
+}
+//===========
+
+
+ if($textmessage == '/start')
+{
+SendMessage($chat_id,"*Welcome* `#$name` :)\nYou Can Change This ;)");
+}
+elseif ($textmessage == '/fwrd')
+{
+SendMessage($chat_id,"Now I Forward Your Text To You :X");
+Forward($chat_id,$chat_id,$message_id);
+}
+elseif ($textmessage == '/inlinekb')
+{
+var_dump(makereq('sendMessage',[
+        'chat_id'=>$update->message->chat->id,
+        'text'=>"_This Is Simple Inline Keyboard Only For Only for_ *training*",
+	'parse_mode'=>'MarkDown',
+        'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+                [
+                    ['text'=>"Join SpyGuard Channel 👑",'url'=>"https://telegram.me/SpyGuard"]
+                ]
+            ]
+        ])
+    ]));
 }
 else
 {
-SendMessage($chat_id,"Command Not Found");
+SendMessage($chat_id,"*Command Not Found*");
 }
+
 
 ?>
