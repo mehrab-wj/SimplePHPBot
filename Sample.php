2bf8a9153a3442bf1fc5a66ffaa8fa931927234e
mehrab-wj <white_joker20@yahoo.com>
2016-08-03 12:14:08 -0700

    Update Sample.php

--- Sample.php
+++ Sample.php
@@ -8,20 +8,6 @@
 ############################################################################################
 */
 define('API_KEY','BOTTOKEN');
-function makereq($method,$datas=[]){
-    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
-    $res = curl_exec($ch);
-    if(curl_error($ch)){
-        var_dump(curl_error($ch));
-    }else{
-        return json_decode($res);
-    }
-}
-
 //----######------
 
 function makereq($method,$datas=[]){
