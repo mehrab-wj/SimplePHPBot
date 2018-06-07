c715077a9625eb1866cfe4172eac6fdea848122a
iMehrab <white_joker20@yahoo.com>
2018-06-08 02:49:00 +0430

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,38 +23,6 @@ function makereq($method,$datas=[]){
         return json_decode($res);
     }
 }
-
-//##############=--API_REQ
-function apiRequest($method, $parameters) {
-  if (!is_string($method)) {
-    error_log("Method name must be a string\n");
-    return false;
-  }
-
-  if (!$parameters) {
-    $parameters = array();
-  } else if (!is_array($parameters)) {
-    error_log("Parameters must be an array\n");
-    return false;
-  }
-
-  foreach ($parameters as $key => &$val) {
-    // encoding to JSON array parameters, for example reply_markup
-    if (!is_numeric($val) && !is_string($val)) {
-      $val = json_encode($val);
-    }
-  }
-  $url = "https://api.telegram.org/bot".API_KEY."/".$method.'?'.http_build_query($parameters);
-
-  $handle = curl_init($url);
-  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
-  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
-
-  return exec_curl_request($handle);
-}
-
-//----######------
 //---------
 $update = json_decode(file_get_contents('php://input'));
 var_dump($update);
