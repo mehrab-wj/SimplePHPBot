6bef7da63a6dd3f3fa4213e5735af445d8046425
Amir <amir.advance92@gmail.com>
2021-06-18 02:30:49 +0430

    make it better

--- bot.php
+++ bot.php
@@ -27,9 +27,9 @@ function makereq($method,$datas=[]){
 $update = json_decode(file_get_contents('php://input'));
 var_dump($update);
 //=========
-$chat_id = @$update->message->chat->id;
+@$chat_id = $update->message->chat->id ? $update->edited_message->chat->id;
 $message_id = @$update->message->message_id;
-$from_id = @$update->message->from->id;
+@from_id = $update->message->from->id ? $update->edited_message->from->id;
 $name = @$update->message->from->first_name;
 $username = @$update->message->from->username;
 $textmessage = @$update->message->text?:$update->edited_message->text?:$update->channel_post->text;
